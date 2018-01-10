bff702e6f6a52157b10a0f463c7cd9310b8d4145
lvzhenwei <lvzhenwei@vcinema.cn>
2018-01-10 23:02:17 +0800

    rxandroiddemo

---
+++ RxAndroid2Demo/app/src/main/java/com/example/lvzhenwei/rxandroid2demo/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.lvzhenwei.rxandroid2demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //创建被观察者
+
+    //创建观察者
+    
+    //订阅
+
+}
